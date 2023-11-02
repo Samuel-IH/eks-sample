c9f51c9acc5b58efa2c8ad5f0dcc790d14cc7be6
Samuel Hart <46057508+Samuel-IH@users.noreply.github.com>
2023-11-02 10:47:43 -0500

    Add schema to SlotService

--- gen-schema.ps1
+++ gen-schema.ps1
@@ -1,1 +1,7 @@
+# commands are split in case of future customization on a per-target basis
+
+# Unity
 docker run -v "$(pwd):/input" algas/quicktype /input/Schema/types.json -o /input/SlotMachine/Assets/Scripts/Schema/Types.cs --lang csharp --namespace Schema
+
+# SlotService (.NET)
+docker run -v "$(pwd):/input" algas/quicktype /input/Schema/types.json -o /input/SlotService/SlotService/Schema/Types.cs --lang csharp --namespace Schema
